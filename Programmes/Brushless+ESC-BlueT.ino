c2c725e308e5289575d259f392ebcc84c791d794
Mofid et Polette <45670769+MofidPolette@users.noreply.github.com>
2019-02-12 19:49:39 +0100

    Update and rename Brushless+ESC.ino to Brushless+ESC-BlueT.ino

--- Programmes/Brushless+ESC-BlueT.ino
+++ Programmes/Brushless+ESC-BlueT.ino
@@ -1,13 +1,20 @@
 #include <Servo.h>
- 
+#include <SoftwareSerial.h>
+#define RX 10
+#define TX 11
 Servo esc;   // Création de l'objet permettant le contrôle de l'ESC
- 
+char Data;
+SoftwareSerial BlueT(RX,TX);
 int val = 0; // 
  
 void setup() {
-   esc.attach(3); // On attache l'ESC au port numérique 9 (port PWM obligatoire)
+   esc.attach(9); // On attache l'ESC au port numérique 9 (port PWM obligatoire)
    delay(15);
    Serial.begin(9600);
+   delay(15);
+   Serial.println("Salut, je suis prêt pour l'AT !");
+   BlueT.begin(9600);
+   delay(500);
  
    // Initialisation de l'ESC
    //  (certains ESC ont besoin d'une "procédure d'initialisation"
@@ -21,14 +28,29 @@ void setup() {
    // Quelques informations pour l'utilisateur
    Serial.println("Saisir un nombre entre 0 et 179");
    Serial.println("(0 = arret - 179 = vitesse maxi");
-   Serial.println(" demarrage a partir de 20)");
+   Serial.println(" demarrage a partir de 44)");
    }
  
 void loop() {
-   if (Serial.available() > 0) {
+   /*if (Serial.available() > 0) {
       val = Serial.parseInt();   // lecture de la valeur passée par le port série
       Serial.println(val);
       esc.write(val);            // 
       delay(15);
-      }
+      }*/
+    Data=BlueT.read();
+    if (Data=='M'){
+      val = BlueT.parseInt();
+      Serial.println(val);
+      esc.write(val);
+      delay(15);
+      Data=BlueT.read();
+    }
+
+
+
+
+
+
+      
    }
