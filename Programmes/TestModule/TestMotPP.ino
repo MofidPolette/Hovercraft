7905dee36b2a57bb1add27b86370ea08107acb92
Mofid et Polette <45670769+MofidPolette@users.noreply.github.com>
2019-03-09 13:58:35 +0100

    Update TestMotPP.ino

--- Programmes/TestModule/TestMotPP.ino
+++ Programmes/TestModule/TestMotPP.ino
@@ -1,13 +1,13 @@
 #include <Stepper.h>
 
-const int nombrePas = 32*64;
-Stepper monMoteur(nombrePas, 2, 4, 3, 5);
+const int nombrePas = 32*64;//dfifférent selon les modèles
+Stepper monMoteur(nombrePas, 2, 4, 3, 5);//faire attention à l'ordre des pin : (IN1, IN3, IN2, IN4)
 
 void setup() {
   monMoteur.setSpeed(10);
 }
 
-void loop() {
+void loop() { //fait des mouvements dans les 2 sens
   monMoteur.step(10000);
   delay(5000);
 
