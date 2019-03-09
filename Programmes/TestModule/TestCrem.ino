19fc4383928a42dc627d046bbdb082b108d4c699
Mofid et Polette <45670769+MofidPolette@users.noreply.github.com>
2019-03-09 13:49:35 +0100

    Update TestCrem.ino

--- Programmes/TestModule/TestCrem.ino
+++ Programmes/TestModule/TestCrem.ino
@@ -4,8 +4,8 @@
 #define TX 11
 char Data ;
 SoftwareSerial BlueT(RX, TX);
-const int nombrePas = 32*64;
-Stepper monMoteur(nombrePas, 2, 4, 3, 5);
+const int nombrePas = 32*64; //dfifférent selon les modèles
+Stepper monMoteur(nombrePas, 2, 4, 3, 5); //faire attention à l'ordre des pin : (IN1, IN3, IN2, IN4)
 
 void setup() {
   Serial.begin(9600);
@@ -13,8 +13,8 @@ void setup() {
   delay(500);
 }
 
-void loop() {
-  monMoteur.step(-100);
+void loop() { //fait des mouvements dans les 2 sens
+  monMoteur.step(-100); 
   delay(500);
   monMoteur.step(100);
 }
